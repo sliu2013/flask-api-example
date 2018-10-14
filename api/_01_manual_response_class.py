0c14b982f88be5655d312c36bd6bfe795ef09765
Sky Liu <skyl@vmware.com>
2018-10-13 21:48:57 -0700

    example to set opentracing.extract in a Flask- api-server (server side)

--- api/_01_manual_response_class.py
+++ api/_01_manual_response_class.py
@@ -1,6 +1,34 @@
 import json
-from flask import Flask, Response, abort
+from flask import Flask, Response, abort, request
 from .utils import JSON_MIME_TYPE, search_book
+
+from wavefront_python_sdk import WavefrontDirectClient, WavefrontProxyClient
+from wavefront_opentracing_python_sdk import WavefrontTracer
+from wavefront_opentracing_python_sdk.reporting import CompositeReporter, \
+    WavefrontSpanReporter, ConsoleReporter
+from opentracing.propagation import Format
+
+# Report opentracing spans to Wavefront via a Wavefront Direct Ingestion.
+direct_client = WavefrontDirectClient(
+    server="<wavefront-server>",
+    token="<token>"
+)
+direct_reporter = WavefrontSpanReporter(
+    client=direct_client,
+    source="wavefront-sky-direct"
+)
+# Create Wavefront Span Reporter using Wavefront Proxy Client.
+proxy_client = WavefrontProxyClient(
+    host="localhost",
+    tracing_port=30000,
+    distribution_port=40000,
+    metrics_port=2878
+)
+proxy_reporter = WavefrontSpanReporter(proxy_client,
+                                       source='wavefront-sky-proxy')
+composite_reporter = CompositeReporter(direct_reporter, proxy_reporter,
+                                       ConsoleReporter('wavefront-sky'))
+tracer = WavefrontTracer(reporter=composite_reporter)
 
 app = Flask(__name__)
 
@@ -13,9 +41,24 @@ books = [{
 
 @app.route('/book')
 def book_list():
-    response = Response(
-        json.dumps(books), status=200, mimetype=JSON_MIME_TYPE)
-    return response
+    span = before_request(request, tracer, 'book_list')
+    with tracer.scope_manager.activate(span, True) as scope:
+        response = Response(
+            json.dumps(books), status=200, mimetype=JSON_MIME_TYPE)
+        return response
+
+
+def before_request(request1, tracer1, operation_name):
+    span_context = tracer1.extract(
+        format=Format.HTTP_HEADERS,
+        carrier=dict(request1.headers)
+    )
+    span = tracer1.start_span(
+        operation_name=operation_name,
+        child_of=span_context)
+    span.set_tag('http.url', request1.base_url)
+    span.set_tag('service', 'flask_api_server')
+    return span
 
 
 @app.route('/book/<int:book_id>')
@@ -31,3 +74,5 @@ def book_detail(book_id):
 @app.errorhandler(404)
 def not_found(e):
     return '', 404
+
+
--- run_app.py
+++ run_app.py
@@ -11,5 +11,5 @@ if __name__ == '__main__':
     app.debug = True
     app.config['DATABASE_NAME'] = 'library.db'
     host = os.environ.get('IP', '0.0.0.0')
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 7080))
     app.run(host=host, port=port)
